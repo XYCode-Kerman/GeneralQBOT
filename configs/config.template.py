d4f5822b4362e7457b97384080a702840ff54633
XYCode <xycode-xyc@outlook.com>
2023-05-03 08:34:06 +0800

    不保存配置文件，但是提供配置文件的模板

---
+++ configs/config.template.py
@@ -0,0 +1,15 @@
+# Mirai Api HTTP
+API_VERIFY_KEY = ''
+API_HOST = ''
+API_PORT = 0
+
+# admin
+ADMIN_QQ = []
+
+# bot
+BOT_QQ = 0
+STARTS_WITH = '.'
+
+# Tencent Cloud
+QCLOUD_SECRET_ID = ''
+QCLOUD_SECRET_KEY = ''
