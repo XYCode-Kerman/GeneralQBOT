cd3e6ad5c6bc8155cfbba986ae18896e512a263c
XYCode <xycode-xyc@outlook.com>
2023-05-04 13:03:36 +0800

    代码格式化

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -8,7 +8,7 @@ from mirai import *
 
 async def test(event: Union[GroupMessage, FriendMessage], bot: Mirai, command: List[str]):
     await bot.send(event, [Plain('这是一个测试，出现这条消息，说明机器人已经在工作了！')])
-    
+
 async def math_handle(event: GroupMessage, bot: Mirai, command: List[str]):
     del command[0]
     print(command[0])
