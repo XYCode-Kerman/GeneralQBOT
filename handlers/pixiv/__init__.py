ad799800400f66c2a2ecfc91cd82e5d51ce99fb8
XYCode <xycode-xyc@outlook.com>
2023-05-16 13:31:36 +0800

    pixiv的tag监听器和管理系统

---
+++ handlers/pixiv/__init__.py
@@ -0,0 +1,6 @@
+"""
+* @project       GeneralQBOT
+* @author        XYCode-Kerman <xycode-xyc@outlook.com>
+* @date          1970-01-01 08:00:00
+* @lastModified  2023-05-15 13:04:12
+"""
---
+++ utils/logger.py
@@ -0,0 +1,23 @@
+"""
+* @project       GeneralQBOT
+* @author        XYCode-Kerman <xycode-xyc@outlook.com>
+* @date          1970-01-01 08:00:00
+* @lastModified  2023-05-15 22:46:21
+"""
+import logging
+import datetime
+
+def get_gq_logger() -> logging.Logger:
+    logger = logging.getLogger('GeneralQBOT')
+    
+    logging.basicConfig(
+        filename=f'./logs/{datetime.datetime.now().timestamp()}.log',
+        filemode='w'
+    )
+    
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('[%(asctime)s] [%(pathname)s] [line:%(lineno)d] %(message)s')
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    return logger
