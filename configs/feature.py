6de472868b7f544fc03d5c4e1e7215e98ed94327
XYCode <xycode-xyc@outlook.com>
2023-05-26 20:44:40 +0800

    新增关于面试的功能启用

--- configs/feature.py
+++ configs/feature.py
@@ -4,6 +4,8 @@ class Features(Enum):
     TencentTMS = auto()
     LocalAITMS = auto()
     LocalKeywordTMS = auto()
+    Interview = auto()
 
 # 推荐使用TencentTMS，LocalAI由于数据集不充分，且训练步数较少，不认识某些生词，准确率大概在50%左右
 ENABLE_TMS_SERVICER = [Features.TencentTMS, Features.LocalAITMS]
+ENABLED_FEATURE = [Features.Interview]
