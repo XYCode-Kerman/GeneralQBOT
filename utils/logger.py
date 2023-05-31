c2908078f68a7ac8f679316e6f069167736d9d66
XYCode <xycode-xyc@outlook.com>
2023-05-31 23:41:28 +0800

    bugfix：同一log多次打印

--- utils/logger.py
+++ utils/logger.py
@@ -7,17 +7,18 @@
 import logging
 import datetime
 
+logger = logging.getLogger('GeneralQBOT')
+
+logging.basicConfig(
+    filename=f'./logs/{datetime.datetime.now().timestamp()}.log',
+    filemode='w'
+)
+
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(asctime)s] [%(pathname)s] [line:%(lineno)d] %(message)s')
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
 def get_gq_logger() -> logging.Logger:
-    logger = logging.getLogger('GeneralQBOT')
-    
-    logging.basicConfig(
-        filename=f'./logs/{datetime.datetime.now().timestamp()}.log',
-        filemode='w'
-    )
-    
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('[%(asctime)s] [%(pathname)s] [line:%(lineno)d] %(message)s')
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
     return logger
