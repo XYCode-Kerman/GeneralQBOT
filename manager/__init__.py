67a6663b518bbff32f7c17cd55e80df3eb0adb8b
XYCode <xycode-xyc@outlook.com>
2023-06-07 12:04:27 +0800

    🎁新增：在/ping中对是否已经登录的判断

--- manager/__init__.py
+++ manager/__init__.py
@@ -1,10 +1,12 @@
 import flask
 import jwt
 from datetime import timedelta, datetime
-from flask import Request, Response, request
+from flask import Request, Response, request, make_response
 from configs import config
+from utils.token import verify_token
 
 app = flask.Flask(__name__)
+
 
 @app.route('/ping')
 async def ping():
@@ -14,26 +16,35 @@ async def ping():
         'client': {
             'args': request.args.to_dict(),
             'method': request.method,
-            'cookies': request.cookies
+            'cookies': request.cookies,
+            'login': verify_token(request.cookies.get('token', '')),
         }
     }
+
 
 @app.route('/auth/login', methods=['POST'])
 async def auth_login():
     if request.is_json:
         username = request.get_json().get('username')
         password = request.get_json().get('password')
-        
+
         if username == config.REMOTE_MANAGER_ADMIN_NAME and password == config.REMOTE_MANAGER_ADMIN_PASSWORD:
-            return {
+            token = jwt.encode({
+                'username': username,
+                'password': password,
+                'expires':  (datetime.now() + timedelta(days=1)).timestamp()
+            }, config.JWT_KEY)
+
+            resp = make_response({
                 'status': 200,
                 'message': 'success',
-                'token': jwt.encode({
-                    'username': username,
-                    'password': password,
-                    'expires':  datetime.now() + timedelta(days=1) 
-                }, config.JWT_KEY)
-            }, 200
+                'token': token
+            })
+            
+            resp.status_code = 200
+            resp.set_cookie('token', token)
+            
+            return resp
         else:
             return {
                 'status': 403,
---
+++ utils/token.py
@@ -0,0 +1,17 @@
+import jwt
+from datetime import timedelta, datetime
+from typing import Any, Callable, Dict
+from configs.config import JWT_KEY
+
+def verify_token(token: str) -> bool:
+    try:
+        data: Dict[str, Any] = jwt.decode(token, JWT_KEY)
+        expires = data.get('expires', (datetime.now() + timedelta(days=1)).timestamp())
+        expires = datetime.fromtimestamp(expires)
+        
+        if expires < datetime.now():
+            return False
+        else:
+            return True
+    except:
+        return False
