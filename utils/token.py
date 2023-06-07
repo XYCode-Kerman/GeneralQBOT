d0f3574211deb8542d7c9758602d3ba1bc578e4a
XYCode <xycode-xyc@outlook.com>
2023-06-07 16:34:07 +0800

    🐛修复：verify_token函数始终返回False

--- utils/token.py
+++ utils/token.py
@@ -4,8 +4,9 @@ from typing import Any, Callable, Dict
 from configs.config import JWT_KEY
 
 def verify_token(token: str) -> bool:
+    
     try:
-        data: Dict[str, Any] = jwt.decode(token, JWT_KEY)
+        data: Dict[str, Any] = jwt.decode(token, JWT_KEY, 'HS256')
         expires = data.get('expires', (datetime.now() + timedelta(days=1)).timestamp())
         expires = datetime.fromtimestamp(expires)
         
