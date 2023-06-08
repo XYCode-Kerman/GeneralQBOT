58c87332f70765771ca25f6b05d4cab9fbe8d9a2
XYCode <xycode-xyc@outlook.com>
2023-06-08 12:27:44 +0800

    🎁新增：远程管理  发送消息

--- manager/messages.py
+++ manager/messages.py
@@ -1,12 +1,12 @@
 import flask
 import json
-from mirai import Mirai
-from typing import Union
+from mirai import Mirai, MessageChain
+from typing import Union, Literal
 from utils.token import verify_token
 from utils.database import get_col
 from utils.logger import get_gq_logger
 from flask import make_response, request, Blueprint
-from bson.json_util import dumps
+from bson.json_util import dumps, loads
 
 messages_blueprint = Blueprint(
     'messages',
@@ -15,7 +15,7 @@ messages_blueprint = Blueprint(
 )
 
 logger = get_gq_logger()
-mirai: Union[Mirai, None] = None
+mirai: Mirai = None
 
 @messages_blueprint.route('/list', methods=['GET'])
 def list_messages():
@@ -32,4 +32,45 @@ def list_messages():
     
     return data, 200
 
-# @messages_blueprint.route('/send', methods)
+@messages_blueprint.route('/send', methods=['POST'])
+async def send_message():
+    global mirai
+    
+    if verify_token(request.cookies.get('token', '')) == False:
+        return {
+            'status': 403,
+            'message': 'forbidden'
+        }, 403
+    
+    target = request.get_json().get('target', '')
+    target_type: Literal['group', 'friend'] = request.get_json().get('target_type', '')
+    message_chain = request.get_json().get('message_chain', [])
+
+    # 判断请求是否正确
+    if target == '' or target_type == '' or message_chain == []:
+        return {
+            'status': 400,
+            'message': 'bad request'
+        }, 400
+    else:
+        message_chain = MessageChain(message_chain)
+        
+        if target_type == 'group':
+            await mirai.send_group_message(int(target), message_chain)
+
+            return {
+                'status': 200,
+                'message_chain': message_chain.dict()
+            }, 200
+        elif target_type == 'friend':
+            await mirai.send_friend_message(int(target), message_chain)
+
+            return {
+                'status': 200,
+                'message_chain': message_chain.dict()
+            }, 200
+        else:
+            return {
+                'status': 400,
+                'message': 'bad request'
+            }, 400
